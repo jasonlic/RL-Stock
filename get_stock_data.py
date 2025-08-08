ea7fe74a1817e7b018b54f9cb6c6e1bca12f2e37
jason_li_c <jason_li_c@hotmail.com>
2025-08-09 07:21:08 +0800

    add jump over files we have gotton

--- get_stock_data.py
+++ get_stock_data.py
@@ -38,8 +38,16 @@ class Downloader(object):
 
     def run(self):
         stock_df = self.get_codes_by_date(self.date_end)
-        for index, row in stock_df.iterrows():
-            if '*' in row["code_name"]:
+        ''''
+        #to jump over files we have gotten
+        target_str = "首开股份"
+        mask = stock_df['code_name'].str.contains(target_str)
+        start_idx = stock_df[mask].index[0] 
+        print(mask,start_idx)
+        '''
+        start_idx = 0
+        for index, row in stock_df.loc[start_idx:].iterrows(): #stock_df.iterrows():
+            if '*' in row["code_name"]: #jump over nmaes which has *
                 print('jump over', row["code_name"])
                 continue
             print(f'processing {row["code"]} {row["code_name"]}')
@@ -47,16 +55,17 @@ class Downloader(object):
                                                    start_date=self.date_start,
                                                    end_date=self.date_end).get_data()
             df_code.to_csv(f'{self.output_dir}/{row["code"]}.{row["code_name"]}.csv', index=False)
+
         self.exit()
 
 
 if __name__ == '__main__':
     # 获取全部股票的日K线数据
     mkdir('./stockdata/train')
-    downloader = Downloader('./stockdata/train', date_start='1990-01-01', date_end='2019-11-29')
+    downloader = Downloader('./stockdata/train', date_start='1990-01-01', date_end='2025-08-06')
     downloader.run()
 
     mkdir('./stockdata/test')
-    downloader = Downloader('./stockdata/test', date_start='2019-12-01', date_end='2019-12-31')
+    downloader = Downloader('./stockdata/test', date_start='2025-07-01', date_end='2025-08-06')
     downloader.run()
 
